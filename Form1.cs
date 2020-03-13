6a3043bd9db64d9e8dcc05089c49f53272ad8457
Tbbx97 <55205826+Tbbx97@users.noreply.github.com>
2020-03-13 16:38:47 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string tt;
+            tt = "\n Tên Khách Hàng:    " + tenkh.Text;
+            ///tt += "\n Loại Phòng:";
+            if (pd1.Checked == true)
+                tt += "\n Loại Phòng:   Phòng Đơn:  " + "Đơn Gía:   " + dg1.Text + "Số Lượng:   " +sl1.Value;
+                
+            else
+                tt += "\nLoại Phòng:    Phòng Đôi:  " + "Đơn Gía:   " + dg2.Text + "Số Lượng:   " + sl2.Value;
+            tt += "\n Ngày Đến:   " + nd1.Value.ToShortDateString();
+            tt += "\n Ngày Đi:   " + nd2.Value.ToShortDateString();
+            tt += "\n Số Ngày Ở:  " + sno.Text;
+            tt += "\n Tổng Tiền: " + tongtien.Text;
+
+            richTextBox1.Text = tt;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void sl1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
